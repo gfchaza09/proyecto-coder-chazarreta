6822e67c76d54e77853a7f5a00cc9797e4eea5d5
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-08-05 00:48:09 -0300

    Agregando fetch a eventos.js y carta.js

--- js/carta.js
+++ js/carta.js
@@ -18,36 +18,44 @@ const generateCards = (arrObjetos, container) => {
     }
 }
 
-const pizzas = [
-    {id: 1, titulo: "Muzzarella", parrafo: "Muzzarella, aceitunas y orégano", precio: 620, img:"../images/muzzarella.jpg"},
-    {id: 2, titulo: "Jamón y Morrón", parrafo: "Muzzarella, jamón, morrones, aceitunas y orégano", precio: 720, img:"../images/jamonymorron.jpg"},
-    {id: 3, titulo: "Napolitana", parrafo: "Muzzarella, rodajas de tomate y orégano", precio: 720, img:"../images/napolitana.jpg"},
-];
+// Se reemplazaron por el simulador de API mocki
+// const pizzas = [
+//     {id: 1, titulo: "Muzzarella", parrafo: "Muzzarella, aceitunas y orégano", precio: 620, img:"../images/muzzarella.jpg"},
+//     {id: 2, titulo: "Jamón y Morrón", parrafo: "Muzzarella, jamón, morrones, aceitunas y orégano", precio: 720, img:"../images/jamonymorron.jpg"},
+//     {id: 3, titulo: "Napolitana", parrafo: "Muzzarella, rodajas de tomate y orégano", precio: 720, img:"../images/napolitana.jpg"},
+// ];
 
-const empanadas = [
-    {id: 101, titulo: "Carne", precio: 80, img:"../images/empanada_carne.jpg"},
-    {id: 102, titulo: "Jamón y Queso", precio: 80, img:"../images/empanada_jamonyqueso.jpg"},
-    {id: 103, titulo: "Atún", precio: 80, img:"../images/empanada_atun.jpg"},
-];
+// const empanadas = [
+//     {id: 101, titulo: "Carne", precio: 80, img:"../images/empanada_carne.jpg"},
+//     {id: 102, titulo: "Jamón y Queso", precio: 80, img:"../images/empanada_jamonyqueso.jpg"},
+//     {id: 103, titulo: "Atún", precio: 80, img:"../images/empanada_atun.jpg"},
+// ];
 
-const bebidas = [
-    {id: 201, titulo: "Agua", parrafo: "Marca: Nestlé", precio: 150, img:"../images/agua.jpg"},
-    {id: 202, titulo: "Gaseosa", parrafo: "Marca: Pepsi", precio: 300, img:"../images/gaseosa.jpg"},
-    {id: 203, titulo: "Cerveza", parrafo: "Marca: Brahma", precio: 350, img:"../images/cerveza.jpg"},
-];
+// const bebidas = [
+//     {id: 201, titulo: "Agua", parrafo: "Marca: Nestlé", precio: 150, img:"../images/agua.jpg"},
+//     {id: 202, titulo: "Gaseosa", parrafo: "Marca: Pepsi", precio: 300, img:"../images/gaseosa.jpg"},
+//     {id: 203, titulo: "Cerveza", parrafo: "Marca: Brahma", precio: 350, img:"../images/cerveza.jpg"},
+// ];
 
-const postres = [
-    {id: 301, titulo: "Bombones", precio: 80, img:"../images/bombones.jpg"},
-    {id: 302, titulo: "Almendrado", precio: 140, img:"../images/almendrado.jpg"},
-    {id: 303, titulo: "Helado", precio: 100, img:"../images/helado.jpg"},
-];
+// const postres = [
+//     {id: 301, titulo: "Bombones", precio: 80, img:"../images/bombones.jpg"},
+//     {id: 302, titulo: "Almendrado", precio: 140, img:"../images/almendrado.jpg"},
+//     {id: 303, titulo: "Helado", precio: 100, img:"../images/helado.jpg"},
+// ];
 
 const cardsPizzas = document.getElementById("pizzas");
 const cardsEmpanadas = document.getElementById("empanadas");
 const cardsBebidas = document.getElementById("bebidas");
 const cardsPostres = document.getElementById("postres");
 
-generateCards(pizzas, cardsPizzas);
-generateCards(empanadas, cardsEmpanadas);
-generateCards(bebidas, cardsBebidas);
-generateCards(postres, cardsPostres);
+// generateCards(pizzas, cardsPizzas);
+
+fetch('https://mocki.io/v1/9073e396-ec19-4bff-8ffc-6f36c58af02b')
+    .then(res => res.json())
+    .then(data => {
+        generateCards(data.pizzas, cardsPizzas);
+        generateCards(data.empanadas, cardsEmpanadas);
+        generateCards(data.bebidas, cardsBebidas);
+        generateCards(data.postres, cardsPostres);
+    })
+    .catch(error => console.log("Ocurrió un error:",error));
--- js/eventos.js
+++ js/eventos.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const generateArticles = () => {
+const generateArticles = (eventos) => {
 
     for (const {titulo, altImg, descripcion, fecha, id, img, modal} of eventos) {
 
@@ -49,49 +49,56 @@ const generateArticles = () => {
     };
 };
 
-const eventos = [
-  {
-    id: "event1",
-    titulo: "Presentación carta primavera-verano",
-    fecha: "02/09",
-    img: "https://images.unsplash.com/photo-1600628421060-939639517883?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwyfHxwaXp6YSUyMHBhcnR5fGVufDB8fHx8MTY0OTI3NDYwMA&ixlib=rb-1.2.1&q=80&w=1080",
-    altImg: "presentacion-carta",
-    descripcion: 'La primer semana de septiembre inicia con la presentación de nuestra nueva carta "adapatada a las tendenicas mundiales" que incluye pizzas sin TACC, con harinas orgánicas y nuevos platos.',
-    modal: [
-        {texto: "Parte de la oferta gastronómica incluye pizzas sin TACC (de proovedor certificado), empanadas y opciones pensadas para compartir: tablas de fiambres, entradas deliciosas y pizzas rellenas.", id: 1},
-        {texto: "Pero no solo eso: esta carta trae como novedad la incorporación de nuevos platos y variedades de pizzas, enalteciendo el producto de la mano de productores locales. Se incorporó harina 100% integral orgánica certificada y la posibilidad de que el comensal pueda pedir cualquier pizza con esa versión.", id: 2},
-        {texto: "Como innovación, se incorpora el Khachapuri, un producto novedoso, una especie de pan relleno de mozzarella y otros agregados, para compartir y untar los bordes entre varios comensales.", id: 3}
-    ],
-  },
-  {
-    id: "event2",
-    titulo: "Día de la primavera",
-    fecha: "21/09",
-    img: "https://images.unsplash.com/photo-1533777419517-3e4017e2e15a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxMHx8cGl6emElMjBwYXJ0eXxlbnwwfHx8fDE2NDkyNzQ2MDA&ixlib=rb-1.2.1&q=80&w=1080",
-    altImg: "primavera-pizza",
-    descripcion: 'Vení con toda la familia a disfrutar de nuestra comida en este día especial que contará con descuentos, sorteos y más sorpresas.',
-    modal: [
-        {texto: "En el evento gastronómico que se llevara a cabo este 21 de septiembre habrá descuentos de hasta un 30% y promociones para los fanáticos de la muzarella.", id:101},
-        {texto: "La iniciativa tiene como objetivo celebrar la llegada del día de la primavera y que mejor, que degustar unas sabrosas pizzas.", id:102},
-        {texto: "Acercate a nuestros locales, a partir de este mes contamos con libre circulación y 100% de aforo en el interior de nuestros locales. ¿Te lo vas a perder?", id:103}
-    ],
-  },
-  {
-    id: "event3",
-    titulo: "Noche de la Pizza y la Empanada",
-    fecha: "28/09",
-    img: "https://images.unsplash.com/photo-1564936281659-ba4f75a41956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwyfHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080",
-    altImg: "noche-de-pizza",
-    descripcion: 'El martes 28 de septiembre se celebra la edición de La Noche de la Pizza y La Empanada en todo el territorio argentino, un evento único que tiene como objetivo dar impulso al consumo en el sector, con importantes descuentos en salón, mesas al aire libre, take away y delivery de los comercios adheridos y una propuesta digital con cocineros invitados.',
-    modal: [
-        {texto: "Habrá para todos los gustos: opciones para delivery, take away y, por supuesto, para comer en los locales y disfrutar de una experiencia gastronómica completa, con promociones e importantes descuentos en todas las modalidades.", id: 201},
-        {texto: "Habrá sorteos, descuentos del 50% en los precios de la carta, y en todos los locales, los beneficios se mantendrán incluso hasta el domingo.", id:202},
-        {texto: "Te esperamos para que puedas disfrutar de las mejores pizzas y empanadas del mundo, ¿qué esperas?", id:203}
-    ],
+// Se reemplazaron por el simulador de API mocki
+// const eventos = [
+//   {
+//     id: "event1",
+//     titulo: "Presentación carta primavera-verano",
+//     fecha: "02/09",
+//     img: "https://images.unsplash.com/photo-1600628421060-939639517883?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwyfHxwaXp6YSUyMHBhcnR5fGVufDB8fHx8MTY0OTI3NDYwMA&ixlib=rb-1.2.1&q=80&w=1080",
+//     altImg: "presentacion-carta",
+//     descripcion: 'La primer semana de septiembre inicia con la presentación de nuestra nueva carta "adapatada a las tendenicas mundiales" que incluye pizzas sin TACC, con harinas orgánicas y nuevos platos.',
+//     modal: [
+//         {texto: "Parte de la oferta gastronómica incluye pizzas sin TACC (de proovedor certificado), empanadas y opciones pensadas para compartir: tablas de fiambres, entradas deliciosas y pizzas rellenas.", id: 1},
+//         {texto: "Pero no solo eso: esta carta trae como novedad la incorporación de nuevos platos y variedades de pizzas, enalteciendo el producto de la mano de productores locales. Se incorporó harina 100% integral orgánica certificada y la posibilidad de que el comensal pueda pedir cualquier pizza con esa versión.", id: 2},
+//         {texto: "Como innovación, se incorpora el Khachapuri, un producto novedoso, una especie de pan relleno de mozzarella y otros agregados, para compartir y untar los bordes entre varios comensales.", id: 3}
+//     ],
+//   },
+//   {
+//     id: "event2",
+//     titulo: "Día de la primavera",
+//     fecha: "21/09",
+//     img: "https://images.unsplash.com/photo-1533777419517-3e4017e2e15a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxMHx8cGl6emElMjBwYXJ0eXxlbnwwfHx8fDE2NDkyNzQ2MDA&ixlib=rb-1.2.1&q=80&w=1080",
+//     altImg: "primavera-pizza",
+//     descripcion: 'Vení con toda la familia a disfrutar de nuestra comida en este día especial que contará con descuentos, sorteos y más sorpresas.',
+//     modal: [
+//         {texto: "En el evento gastronómico que se llevara a cabo este 21 de septiembre habrá descuentos de hasta un 30% y promociones para los fanáticos de la muzarella.", id:101},
+//         {texto: "La iniciativa tiene como objetivo celebrar la llegada del día de la primavera y que mejor, que degustar unas sabrosas pizzas.", id:102},
+//         {texto: "Acercate a nuestros locales, a partir de este mes contamos con libre circulación y 100% de aforo en el interior de nuestros locales. ¿Te lo vas a perder?", id:103}
+//     ],
+//   },
+//   {
+//     id: "event3",
+//     titulo: "Noche de la Pizza y la Empanada",
+//     fecha: "28/09",
+//     img: "https://images.unsplash.com/photo-1564936281659-ba4f75a41956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwyfHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080",
+//     altImg: "noche-de-pizza",
+//     descripcion: 'El martes 28 de septiembre se celebra la edición de La Noche de la Pizza y La Empanada en todo el territorio argentino, un evento único que tiene como objetivo dar impulso al consumo en el sector, con importantes descuentos en salón, mesas al aire libre, take away y delivery de los comercios adheridos y una propuesta digital con cocineros invitados.',
+//     modal: [
+//         {texto: "Habrá para todos los gustos: opciones para delivery, take away y, por supuesto, para comer en los locales y disfrutar de una experiencia gastronómica completa, con promociones e importantes descuentos en todas las modalidades.", id: 201},
+//         {texto: "Habrá sorteos, descuentos del 50% en los precios de la carta, y en todos los locales, los beneficios se mantendrán incluso hasta el domingo.", id:202},
+//         {texto: "Te esperamos para que puedas disfrutar de las mejores pizzas y empanadas del mundo, ¿qué esperas?", id:203}
+//     ],
     
-  },
-];
+//   },
+// ];
 
 const containerEventos = document.getElementById("eventos");
 
-generateArticles();
+fetch('https://mocki.io/v1/e3df8c00-6790-4ccf-869a-96dbcd452bf9')
+    .then(res => res.json())
+    .then(data => {
+        const eventos = data;
+        generateArticles(eventos);
+    })
+    .catch(error => console.log("Ocurrió un error:",error));
