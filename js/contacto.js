d2d9b648b3c61c0a2c94dea50a450a4b342a3ab9
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-08-09 21:50:39 -0300

    Se agregó fetch en galerias.js

--- js/contacto.js
+++ js/contacto.js
@@ -4,10 +4,10 @@
 const validateInput = (input) => {
   switch (input) {
     case nombre:
-      if(input.value.trim().length > 6) {
+      if (input.value.trim().length > 6) {
         return false;
       }
-      return "El nombre es demasiado corto. Se admiten más de 6 caracteres."
+      return "El nombre es demasiado corto. Se admiten más de 6 caracteres.";
     case telefono:
       if (regExpTelefono.test(input.value)) {
         return false;
@@ -19,41 +19,41 @@ const validateInput = (input) => {
       }
       return "El email es inválido.";
     case mensaje:
-      if(input.value.trim().length > 20) {
+      if (input.value.trim().length > 20) {
         return false;
       }
-      return "El mensaje será válido a partir de los 20 caracteres."
+      return "El mensaje será válido a partir de los 20 caracteres.";
   }
-}
+};
 
 // Función para mostrar errores en el formulario
 const showError = (validate, input) => {
   let message = document.createElement("span");
   message.textContent = !validate ? "" : validate;
   message.classList.add("message__error");
-  if(!validate) {
+  if (!validate) {
     input.classList.remove("input__error");
-    if(input.parentElement.lastElementChild.className === "message__error") {
+    if (input.parentElement.lastElementChild.className === "message__error") {
       input.parentElement.lastElementChild.textContent = "";
     }
   } else {
     input.classList.add("input__error");
-    if(input.parentElement.lastElementChild.className === "message__error") {
+    if (input.parentElement.lastElementChild.className === "message__error") {
       input.parentElement.lastElementChild.textContent = validate;
       return;
     }
     input.parentElement.append(message);
   }
-}
+};
 
 // Función para vaciar los input cuando se envía exitosamente el mensaje
 const clearInput = () => {
-  inputNombre.value = '';
-  inputTelefono.value = '';
-  inputEmail.value = '';
-  inputMensaje.value = '';
+  inputNombre.value = "";
+  inputTelefono.value = "";
+  inputEmail.value = "";
+  inputMensaje.value = "";
   checkboxSub.checked = false;
-}
+};
 
 // Expresiones regulares
 
@@ -64,7 +64,8 @@ const clearInput = () => {
 const regExpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 // Para la validación del número sólo estamos admitiendo números válidos en Argentina
-const regExpTelefono = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/;
+const regExpTelefono =
+  /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/;
 
 // Campos del formulario
 const inputNombre = document.getElementById("nombre");
@@ -82,29 +83,44 @@ const objFormulario = {
   email: "",
   mensaje: "",
   suscripcion: false,
-}
+};
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (!validateInput(inputNombre) && !validateInput(inputEmail) && !validateInput(inputTelefono) && !validateInput(inputMensaje)) {
-
-    emailjs.sendForm('service_f5u1f9i', 'template_630ehjp', form, "rQrzlebn7X-EtP3em")
-        .then(() => {
-            console.log('¡Mensaje enviado!');
-        }, function(error) {
-            console.log('Hubo un error...', error);
-        });
+  if (
+    !validateInput(inputNombre) &&
+    !validateInput(inputEmail) &&
+    !validateInput(inputTelefono) &&
+    !validateInput(inputMensaje)
+  ) {
+    // Se mandará el mensaje a un email determinado dentro de la configuración de emailjs, precisamente en "service", el cuerpo del email es el "template".
+    // El cuarto argumento es el public key
+    emailjs
+      .sendForm(
+        "service_f5u1f9i",
+        "template_630ehjp",
+        form,
+        "rQrzlebn7X-EtP3em"
+      )
+      .then(
+        () => {
+          console.log("¡Mensaje enviado!");
+        },
+        function (error) {
+          console.log("Hubo un error...", error);
+        }
+      );
 
     Toastify({
-      text: 'Muchas gracias por tu mensaje',
+      text: "Muchas gracias por tu mensaje",
       duration: 3000,
       close: true,
-      gravity: 'top',
-      position: 'right',
+      gravity: "top",
+      position: "right",
       style: {
-        background: '#d1e7dd',
-        color: '#0f5132'
-      }
+        background: "#d1e7dd",
+        color: "#0f5132",
+      },
     }).showToast();
 
     showError(validateInput(inputNombre), inputNombre);
@@ -113,7 +129,6 @@ form.addEventListener("submit", e => {
     showError(validateInput(inputMensaje), inputMensaje);
 
     clearInput();
-    
   } else {
     showError(validateInput(inputNombre), inputNombre);
     showError(validateInput(inputEmail), inputEmail);
--- js/galeria.js
+++ js/galeria.js
@@ -1,25 +1,22 @@
 "use strict";
 
 const generateGallery = (arrGallery, container) => {
-    for (const galeria of arrGallery) {
+  for (const galeria of arrGallery) {
+    let galeriaImg = `<h2 class="mobile__title">${galeria.nombre}</h2>`;
 
-        let galeriaImg = `<h2 class="mobile__title">${galeria.nombre}</h2>`;
-
-        for (const {id, imgUrl} of galeria.img) {
-            if (`${galeria.id}1` === id) {
-                galeriaImg += `<div class="carousel-item active">
+    for (const { id, imgUrl } of galeria.img) {
+      if (`${galeria.id}1` === id) {
+        galeriaImg += `<div class="carousel-item active">
                                     <img class="d-flex w-100 card-img" src=${imgUrl} alt=${id} class="d-block w-100" />
-                                </div>`
-                            ;
-            } else {
-                galeriaImg += `<div class="carousel-item">
+                                </div>`;
+      } else {
+        galeriaImg += `<div class="carousel-item">
                                     <img class="d-flex w-100 card-img" src=${imgUrl} alt=${id} class="d-block w-100" />
-                                </div>`
-                            ;
-            }
-        };
+                                </div>`;
+      }
+    }
 
-        let gallery = `
+    let gallery = `
             <div id=${galeria.id} class="carousel slide gallery__photo" data-bs-ride="carousel">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#${galeria.id}" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -40,9 +37,9 @@ const generateGallery = (arrGallery, container) => {
                 </button>
             </div>
         `;
-        container.innerHTML += gallery;
-    }
-}
+    container.innerHTML += gallery;
+  }
+};
 
 const defaultGallery = (gallery, btn) => {
   gallery.classList.add("gallery-active");
@@ -62,46 +59,118 @@ const showGallery = (gallery, arrGallery, btn, arrBtn) => {
   }
 };
 
-const galerias = [
-  { nombre: "Establecimiento", id: "establecimiento", img: [
-    {id: "establecimiento1", imgUrl:"https://images.unsplash.com/photo-1569230516306-5a8cb5586399?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwzfHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "establecimiento2", imgUrl:"https://images.unsplash.com/photo-1593548615309-5a45c504f994?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw5fHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "establecimiento3", imgUrl:"https://images.unsplash.com/photo-1617655699589-64375e5ad9a5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw4fHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "establecimiento4", imgUrl:"https://images.unsplash.com/photo-1564936281659-ba4f75a41956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwyfHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080"}
-  ]},
-  { nombre: "Pizza y Varios", id: "pizzaVarios", img: [
-    {id: "pizzaVarios1", imgUrl:"https://images.unsplash.com/photo-1594007654729-407eedc4be65?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwzfHxwaXp6YXxlbnwwfHx8fDE2NTA4MzAwNzU&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "pizzaVarios2", imgUrl:"https://images.unsplash.com/photo-1590947132387-155cc02f3212?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw1fHxwaXp6YXxlbnwwfHx8fDE2NTA4MzAwNzU&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "pizzaVarios3", imgUrl:"https://images.unsplash.com/photo-1541745537411-b8046dc6d66c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxMHx8cGl6emF8ZW58MHx8fHwxNjUwODMwMDc1&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "pizzaVarios4", imgUrl:"https://images.unsplash.com/photo-1571066811602-716837d681de?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw0fHxwaXp6YXxlbnwwfHx8fDE2NTA4MzAwNzU&ixlib=rb-1.2.1&q=80&w=1080"}
-  ]},
-  { nombre: "Nuestra Gente", id: "nuestraGente", img: [
-    {id: "nuestraGente1", imgUrl:"https://images.unsplash.com/photo-1558138838-76294be30005?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxfHxwaXp6YSUyMGNoZWZ8ZW58MHx8fHwxNjUwODMwNTE4&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "nuestraGente2", imgUrl:"https://images.unsplash.com/photo-1542834291-c514e77b215f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw0fHxwaXp6YSUyMGNoZWZ8ZW58MHx8fHwxNjUwODMwNTE4&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "nuestraGente3", imgUrl:"https://images.unsplash.com/photo-1551888797-e7d1f43d28c2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw1fHxwaXp6YSUyMGNoZWZ8ZW58MHx8fHwxNjUwODMwNTE4&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "nuestraGente4", imgUrl:"https://images.unsplash.com/photo-1593098656835-3276920d948c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxfHxwaXp6YSUyMHdhaXRlcnxlbnwwfHx8fDE2NTA4MzA2MDk&ixlib=rb-1.2.1&q=80&w=1080"}
-  ]},
-  { nombre: "Personas y Personajes", id: "personas", img: [
-    {id: "personas1", imgUrl:"https://images.unsplash.com/photo-1592861956120-e524fc739696?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwyfHxwaXp6YSUyMHBlb3BsZXxlbnwwfHx8fDE2NTA4MzA0NTk&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "personas2", imgUrl:"https://images.unsplash.com/photo-1533777419517-3e4017e2e15a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxMHx8cGl6emElMjBwZW9wbGV8ZW58MHx8fHwxNjUwODMwNDU5&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "personas3", imgUrl:"https://images.unsplash.com/photo-1606787503890-e23f7f79b299?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw2fHxwaXp6YSUyMHBlb3BsZXxlbnwwfHx8fDE2NTA4MzA0NTk&ixlib=rb-1.2.1&q=80&w=1080"},
-    {id: "personas4", imgUrl:"https://images.unsplash.com/photo-1592417817038-d13fd7342605?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw1fHxwaXp6YSUyMHBlb3BsZXxlbnwwfHx8fDE2NTA4MzA0NTk&ixlib=rb-1.2.1&q=80&w=1080"}
-  ]},
-];
-
-const galleryContainer = document.getElementById("galleryContainer");
-
-generateGallery(galerias,galleryContainer);
+// Se reemplazaron por el simulador de API mocki
+// const galerias = [
+//   {
+//     nombre: "Establecimiento",
+//     id: "establecimiento",
+//     img: [
+//       {
+//         id: "establecimiento1",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1569230516306-5a8cb5586399?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwzfHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "establecimiento2",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1593548615309-5a45c504f994?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw5fHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "establecimiento3",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1617655699589-64375e5ad9a5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw4fHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "establecimiento4",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1564936281659-ba4f75a41956?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwyfHxwaXp6ZXJpYXxlbnwwfHx8fDE2NDkyNzEyMzg&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//     ],
+//   },
+//   {
+//     nombre: "Pizza y Varios",
+//     id: "pizzaVarios",
+//     img: [
+//       {
+//         id: "pizzaVarios1",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1594007654729-407eedc4be65?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwzfHxwaXp6YXxlbnwwfHx8fDE2NTA4MzAwNzU&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "pizzaVarios2",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1590947132387-155cc02f3212?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw1fHxwaXp6YXxlbnwwfHx8fDE2NTA4MzAwNzU&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "pizzaVarios3",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1541745537411-b8046dc6d66c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxMHx8cGl6emF8ZW58MHx8fHwxNjUwODMwMDc1&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "pizzaVarios4",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1571066811602-716837d681de?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw0fHxwaXp6YXxlbnwwfHx8fDE2NTA4MzAwNzU&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//     ],
+//   },
+//   {
+//     nombre: "Nuestra Gente",
+//     id: "nuestraGente",
+//     img: [
+//       {
+//         id: "nuestraGente1",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1558138838-76294be30005?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxfHxwaXp6YSUyMGNoZWZ8ZW58MHx8fHwxNjUwODMwNTE4&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "nuestraGente2",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1542834291-c514e77b215f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw0fHxwaXp6YSUyMGNoZWZ8ZW58MHx8fHwxNjUwODMwNTE4&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "nuestraGente3",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1551888797-e7d1f43d28c2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw1fHxwaXp6YSUyMGNoZWZ8ZW58MHx8fHwxNjUwODMwNTE4&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "nuestraGente4",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1593098656835-3276920d948c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxfHxwaXp6YSUyMHdhaXRlcnxlbnwwfHx8fDE2NTA4MzA2MDk&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//     ],
+//   },
+//   {
+//     nombre: "Personas y Personajes",
+//     id: "personas",
+//     img: [
+//       {
+//         id: "personas1",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1592861956120-e524fc739696?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwyfHxwaXp6YSUyMHBlb3BsZXxlbnwwfHx8fDE2NTA4MzA0NTk&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "personas2",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1533777419517-3e4017e2e15a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHwxMHx8cGl6emElMjBwZW9wbGV8ZW58MHx8fHwxNjUwODMwNDU5&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "personas3",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1606787503890-e23f7f79b299?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw2fHxwaXp6YSUyMHBlb3BsZXxlbnwwfHx8fDE2NTA4MzA0NTk&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//       {
+//         id: "personas4",
+//         imgUrl:
+//           "https://images.unsplash.com/photo-1592417817038-d13fd7342605?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzM2NDd8MHwxfHNlYXJjaHw1fHxwaXp6YSUyMHBlb3BsZXxlbnwwfHx8fDE2NTA4MzA0NTk&ixlib=rb-1.2.1&q=80&w=1080",
+//       },
+//     ],
+//   },
+// ];
 
 const btnEstablecimiento = document.getElementById("btnEstablecimiento");
 const btnPizzaVarios = document.getElementById("btnPizzaVarios");
 const btnNuestraGente = document.getElementById("btnNuestraGente");
 const btnPersonas = document.getElementById("btnPersonas");
-
-const establecimiento = document.getElementById("establecimiento");
-const pizzaVarios = document.getElementById("pizzaVarios");
-const nuestraGente = document.getElementById("nuestraGente");
-const personas = document.getElementById("personas");
 
 // Array de botones
 const arrBotones = [
@@ -111,23 +180,46 @@ const arrBotones = [
   btnPersonas,
 ];
 
-// Array de nodos de galerías
-const arrNodoGalerias = [establecimiento, pizzaVarios, nuestraGente, personas];
+fetch("https://mocki.io/v1/b910dc29-e80c-4c45-bb9d-d1854309f337")
+  .then((res) => res.json())
+  .then((data) => {
+    const galleryContainer = document.getElementById("galleryContainer");
 
-btnEstablecimiento.addEventListener("click", () => {
-  showGallery(establecimiento, arrNodoGalerias, btnEstablecimiento, arrBotones);
-});
+    generateGallery(data, galleryContainer);
 
-btnPizzaVarios.addEventListener("click", () => {
-  showGallery(pizzaVarios, arrNodoGalerias, btnPizzaVarios, arrBotones);
-});
+    const establecimiento = document.getElementById("establecimiento");
+    const pizzaVarios = document.getElementById("pizzaVarios");
+    const nuestraGente = document.getElementById("nuestraGente");
+    const personas = document.getElementById("personas");
 
-btnNuestraGente.addEventListener("click", () => {
-  showGallery(nuestraGente, arrNodoGalerias, btnNuestraGente, arrBotones);
-});
+    // Array de nodos de galerías
+    const arrNodoGalerias = [
+      establecimiento,
+      pizzaVarios,
+      nuestraGente,
+      personas,
+    ];
 
-btnPersonas.addEventListener("click", () => {
-  showGallery(personas, arrNodoGalerias, btnPersonas, arrBotones);
-});
+    btnEstablecimiento.addEventListener("click", () => {
+      showGallery(
+        establecimiento,
+        arrNodoGalerias,
+        btnEstablecimiento,
+        arrBotones
+      );
+    });
 
-defaultGallery(establecimiento, btnEstablecimiento);
+    btnPizzaVarios.addEventListener("click", () => {
+      showGallery(pizzaVarios, arrNodoGalerias, btnPizzaVarios, arrBotones);
+    });
+
+    btnNuestraGente.addEventListener("click", () => {
+      showGallery(nuestraGente, arrNodoGalerias, btnNuestraGente, arrBotones);
+    });
+
+    btnPersonas.addEventListener("click", () => {
+      showGallery(personas, arrNodoGalerias, btnPersonas, arrBotones);
+    });
+
+    defaultGallery(establecimiento, btnEstablecimiento);
+  });
